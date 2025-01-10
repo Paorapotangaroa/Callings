cb1eef96716f5b4d9e768b8ab6aae689460c59ba
Paorapotangaroa <92904680+Paorapotangaroa@users.noreply.github.com>
2025-01-10 13:51:01 -0700

    WIP convert G-Sheets App into Node/Express app

---
+++ index.js
@@ -0,0 +1,27 @@
+//Create the port number
+const PORT = 3333;
+let express = require("express");
+let path = require("path");
+let app = express();
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.listen(PORT,()=>console.log("App is listening on port:"+PORT));
+let knex = require("knex")({
+    client: "pg",
+    connection: {
+         host:"localhost",
+         server: "YSA218th",
+         user: "postgres",
+         password: "R3m3mb3r",
+         database: "postgres",
+         port: 5432
+    },
+    useNullAsDefault: true
+});
+
+//send the sign in page
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"home.html"));
+});
+
+
