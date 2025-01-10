a7b3e6738efeef9cbc56929a48dd3d63828e3271
Paorapotangaroa <92904680+Paorapotangaroa@users.noreply.github.com>
2025-01-10 13:51:41 -0700

    Dev server. Hard coding for localhost

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const PORT = 3333;
 let express = require("express");
 let path = require("path");
 let app = express();
-app.use(express.urlencoded({extended:true}));
-app.set("view engine","ejs");
-app.listen(PORT,()=>console.log("App is listening on port:"+PORT));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.listen(PORT, () => console.log("App is listening on port:" + PORT));
 let knex = require("knex")({
     client: "pg",
     connection: {
-         host:"localhost",
-         server: "YSA218th",
-         user: "postgres",
-         password: "R3m3mb3r",
-         database: "postgres",
-         port: 5432
+        host: "localhost",
+        server: "YSA218th",
+        user: "postgres",
+        password: "",
+        database: "postgres",
+        port: 5432
     },
     useNullAsDefault: true
 });
 
 //send the sign in page
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"home.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
 });
-
-
